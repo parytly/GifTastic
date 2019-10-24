fd27015dccf2fc7b2b6d1b5e2ae912a436a38ce5
Pary Ly <paleepary@aol.com>
2019-10-24 14:30:11 -0700

    was able to get the gifs to appear in the html

--- assets/gif.js
+++ assets/gif.js
@@ -1,15 +1,15 @@
 
+//    GIPHY HTTPS LINK:
+   // https://api.giphy.com/v1/gifs/search?api_key=XkIzMsg4ouxg5wPkAxLSXrbHZHZZVWD2&q=&limit=25&offset=0&rating=G&lang=en
+
+//   SAVES THE INPUT UPON CLICKING THE SAVE BUTTON
 $("#save-input").on("click", function(event){
     event.preventDefault();
 
     var search = $("#animal-input").val();
-    // RATINGS: G, PG, PG-13, R
 
     var giphyApiKey = "XkIzMsg4ouxg5wPkAxLSXrbHZHZZVWD2";
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + giphyApiKey + "&limit=10&rating=G"; 
-
-    // https://api.giphy.com/v1/gifs/search?api_key=XkIzMsg4ouxg5wPkAxLSXrbHZHZZVWD2&q=&limit=25&offset=0&rating=G&lang=en
-
     console.log(queryURL);
     console.log(search);
     
@@ -19,11 +19,14 @@ $("#save-input").on("click", function(event){
         }).then(function(response){
         console.log(response);
         
+        var savedButton = $("<button>" + search + "</button>");
+        $("#buttonUpload").append(savedButton);
 
-        var gifImage = $("<img src='" + response.data[0].title + " '> ");
-        $("#animal-gif").append(gifImage);
-       
-
+            // LOOPS THE AVALIABLE GIFS 
+        for (var i = 0; i < response.data.length; i++){
+            var gifImage = $("<img src='" + response.data[i].images.fixed_height.url+ "'> ");
+            $("#animal-gif").append(gifImage);
+        }
     });
 })
 
